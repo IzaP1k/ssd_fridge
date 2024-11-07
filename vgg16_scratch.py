f7cfd9e90e0789338ae3c5595a90bf66e80af6f4
Konradiks <konradrafal.bachor@gmail.com>
2024-11-07 09:28:34 +0100

    aktualizacja z PC-K

--- vgg16_scratch.py
+++ vgg16_scratch.py
@@ -2,22 +2,13 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 import torch
-from torchvision import models
 from torch import nn
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-import torch.nn.functional as F
-import torchvision
 from torch.utils.data import DataLoader, TensorDataset
 
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 
 import os
-import json
-import re
-import random
-from math import sqrt
 
 
 class VGG16(nn.Module):
